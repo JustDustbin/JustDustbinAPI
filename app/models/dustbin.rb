6ffd1b031404910c0b695b5d9c2acb6bc69dacfb
SiDevesh <sid.swapnildevesh@gmail.com>
2017-03-12 14:35:06 +0530

    Remove support_number uniqueness

--- app/models/dustbin.rb
+++ app/models/dustbin.rb
@@ -3,7 +3,7 @@ class Dustbin < ActiveRecord::Base
   validates :name, presence: true, length: { minimum: 1, maximum: 40 }
   validates :worker, presence: true, length: { minimum: 1, maximum: 40 }
   validates :city, presence: true, length: { minimum: 1, maximum: 40 }
-  validates :support_number, phone: { types: [:mobile] }, uniqueness: true, presence: true
+  validates :support_number, phone: { types: [:mobile] }, presence: true
   validates :status, numericality: { only_integer: true, less_than_or_equal_to: 100, greater_than_or_equal_to: 0 }, presence: true
   validates :gps_latitude, presence: true
   validates :gps_longitude, presence: true
